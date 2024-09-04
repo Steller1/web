93687cf9bfd97bb79a320a231f292532edee860d
Steller1 <143689949+Steller1@users.noreply.github.com>
2024-09-04 07:38:44 -0700

    initial commit
    comment

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> About</title>
+    <link rel="stylesheet" href="styles.css"
+</head>
+<body>
+    <header>
+        <img src="images/IMG-20240730-WA0000.jpg" alt="university logo">
+
+    
+        <nav>
+           <a href="home.html">Home</a>
+           <a href="project.html">projects</a>
+           <a href="talent.html">Talent Showcase</a>
+           <a href="about.html">About</a> 
+           <a href="contact.html">Contact</a>
+        
+        </nav>
+    </header>
+    <main>
+        <h1>Welcome to our university Talent and projects Showcase</h1>
+        <p>This website is dedicated to showcasing the diverse talents and innovative project of our students</p>
+    </main>
+    <footer>&copy; 2024 Mbeya university of Science and Technology. All rights reserved</footer>
+
+</body>
+</html>
+    
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>signup form</title>
+    <link rel="stylesheet" href="styles.css"><!--link to the css file-->
+
+</head>
+<body> <div class="signup-container">
+    <form class="signup-form">
+        <div class="signup-header">
+        <h2>signup</h2> 
+        <div class="input-group">
+            <label for="username">Username</label><br>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="input-group">
+            <label for="email">Email</label><br>
+            <input type="mail" id="email" name="email" required>
+        </div>
+        <div class="input-group">
+            <label for="password">Password</label><br>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div class="input-group">
+            <label for="password">Confirm Password</label><br>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+
+        <button type="submit">signup</button>
+        <p class="Login-text">already have an account?<a href="index.html">Login</a></p>
+    </form>
+</div>
+<!-- <footer>&copy; 2024 Mbeya university of Science and Technology. All rights reserved</footer> -->
+</body>
+</html>
+
+    
+</body>
+</html>
